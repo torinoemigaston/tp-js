28284f50181ca174e6f23af47f98ce5897b9ebef
torinoemigaston <miguelthiago335@gmail.com>
2022-11-22 02:08:59 -0300

    Se agraga el tp js

---
+++ script.js
@@ -0,0 +1,55 @@
+//#region
+function suma(num1,num2){
+var suma = num1+num2 ;
+return suma;
+}
+var numero1=2;
+var numero2=4;
+let sumas = suma(numero1,numero2);
+console.log(sumas);
+//#endregion
+//#region
+function restar(num3,num4){
+var restar = num3 - num4;
+return restar;
+}
+var numero3=8;
+var numero4=6;
+let resta= restar(numero3,numero4);
+console.log(resta);
+//#endregion
+//#region
+function division(num5,num6){
+var division = num5/num6;
+return division;
+}
+var numero5=12;
+var numero6=4;
+let dividir = division(numero5,numero6);
+console.log(dividir);
+//#endregion
+//#region
+function multiplicacion(num7,num8){
+var multiplicacion= num7*num8;
+return multiplicacion;
+}
+var numero7=7;
+var numero8=12;
+let multicar= multiplicacion(numero7,numero8);
+
+EPET 20
+Alumno: Emiliano Torino
+Carrera: Técnico Programador
+
+2022
+
+2
+
+console.log(multicar);
+//#endregion
+//#region
+let array=[4,6,8,3,5,2,9];
+for (i=0;i<array.length;i++){
+console.log(array[i]);
+}
+//endregion
